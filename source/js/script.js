c2304c46b3535b3fbf9117b1faba029591d620e9
Liana <pla_alp@bk.ru>
2021-05-23 17:06:00 +0700

    правки после подачи на допуск к защите

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,11 @@
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+
+document.addEventListener("DOMContentLoaded", function () {
+  navMain.classList.remove("main-nav--opened");
+  navMain.classList.add("main-nav--closed");
+});
+
 navMain.classList.remove('main-nav--nojs');
 navToggle.addEventListener('click', function() {
 if (navMain.classList.contains('main-nav--closed')) {
